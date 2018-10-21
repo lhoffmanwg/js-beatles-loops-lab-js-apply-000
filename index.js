223811b398b5f3c565b64e8b4ddb7549b104c15a
Laura Hoffman <lhoffmanwg@gmail.com>
2018-10-21 18:06:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-// add solution here
 
-var i;
-const musicians=["John Lennon","Paul McCartney", "George Harrison","Ringo Starr"];
-const instruments=["Guitar","Bass Guitar","Lead Guitar", "Drums"];
-function theBeatlesPlay(musicians, instruments){
-  var x = [];
-  for (i = 0; i < 4; i++)
-      musicians(i) + "plays" + instruments(i);
-      
-    
-}
